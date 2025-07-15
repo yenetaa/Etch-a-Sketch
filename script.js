9471d2e2b09d6ce9c8f0917aa8473666ab52196f
Yoel <moghosy@gmail.com>
2025-07-15 13:40:59 +0100

    Polish code

--- script.js
+++ script.js
@@ -2,52 +2,54 @@ const gridWrapper = document.querySelector('.grid-wrapper');
 const clearBtn = document.querySelector('.js-clear');
 const editBtn = document.querySelector('.js-edit');
 
+function createGrid(size) {
+  gridWrapper.innerHTML = '';
 
-function createGrid(num) {
-  gridWrapper.innerHTML = ''
-  for (let i = 0; i < (num * num); i++) {
-    let gridSquare = document.createElement('div');
-    gridSquare.className = 'grid-square';
+  const gridWidth = gridWrapper.clientWidth;
+  const squareSize = gridWidth / size;
 
-    let squareSize = 720 / num;
-    gridSquare.style.width = `${squareSize}px`;
-    gridSquare.style.height = `${squareSize}px`;
-    gridWrapper.appendChild(gridSquare);
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
 
-    hoverEffect(gridSquare);
+    square.addEventListener('mouseenter', () => {
+      square.style.backgroundColor = getRandomColor();
+    });
+
+    gridWrapper.appendChild(square);
   }
 }
 
-function hoverEffect(square) {
-  square.addEventListener('mouseenter', () => {
-    const randomColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
-    square.style.backgroundColor = randomColor;
-  })
+function getRandomColor() {
+  const hue = Math.floor(Math.random() * 360);
+  return `hsl(${hue}, 100%, 50%)`;
 }
 
-function getValidNum(min, max, message) {
+function getValidNumber(min, max, message) {
   let num;
   do {
     num = Number(prompt(message));
-  } while (num > max || num < min || isNaN(num))
+  } while (isNaN(num) || num < min || num > max);
   return num;
 }
 
-function editGrid() {
+function setupEditGrid() {
   editBtn.addEventListener('click', () => {
-    const newNum = getValidNum(1, 100, "Enter a number between 1 and 100");
-    console.log(newNum);
-    console.log(createGrid(newNum));
-  })
+    const newSize = getValidNumber(1, 100, "Enter a number between 1 and 100:");
+    createGrid(newSize);
+  });
 }
 
-clearBtn.addEventListener('click', () => {
-  document.querySelectorAll('.grid-square').forEach(square => {
-    square.style.removeProperty('background-color');
+function setupClearGrid() {
+  clearBtn.addEventListener('click', () => {
+    document.querySelectorAll('.grid-square').forEach(square => {
+      square.style.removeProperty('background-color');
+    });
   });
-});
+}
 
 createGrid(16);
-editGrid();
-
-
+setupEditGrid();
+setupClearGrid();
